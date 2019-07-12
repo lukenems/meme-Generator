5c9ecfd6cab3dfbb96269dd90b1474ba6143d2dd
Luke Nemy <lukenemy7@gmail.com>
2019-07-12 12:10:28 -0700

    images still need css tuning, top-text is on top of images now

--- main.js
+++ main.js
@@ -5,10 +5,21 @@ let memeArea = document.querySelector('.meme-area');
 button.addEventListener('click', function(){
     event.preventDefault()
     let imgUrl = document.querySelector('#url').value;
-    console.log('url(' + imgUrl + ');');
+    let top = document.querySelector('#top_text').value;
+    let bottom = document.querySelector('#bottom_text').value;
+
     let newDiv = document.createElement('div');
-    newDiv.style.background = 'url(' +imgUrl+ ')';
+    let image = document.createElement('img')
+    image.src = imgUrl;
+    let p = document.createElement('p')
+    p.innerHTML = top
+
     memeArea.insertAdjacentElement('beforeend', newDiv);
+    newDiv.insertAdjacentElement('beforeend', image);
+    newDiv.insertAdjacentElement('beforeend', p);
+
     document.querySelector('#url').value = '';
+    document.querySelector('#top_text').value = '';
+    document.querySelector('#bottom_text').value = '';
 })
 
