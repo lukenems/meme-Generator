487aa7fe6f990a820bf8c7b06b922106ee314fc8
Luke Nemy <lukenemy7@gmail.com>
2019-07-15 12:00:43 -0700

    text is centerd on images and fixed div/image widths. also selected a font

--- main.js
+++ main.js
@@ -3,20 +3,35 @@ let button = document.querySelector('button');
 let memeArea = document.querySelector('.meme-area');
 
 button.addEventListener('click', function(){
+    console.log(event)
     event.preventDefault()
     let imgUrl = document.querySelector('#url').value;
+    if(imgUrl === ''){
+        return alert("Please enter a URL for the image")
+    }
     let top = document.querySelector('#top_text').value;
     let bottom = document.querySelector('#bottom_text').value;
 
     let newDiv = document.createElement('div');
-    let image = document.createElement('img')
+    newDiv.unselectable = 'on';
+    let image = document.createElement('img');
     image.src = imgUrl;
-    let p = document.createElement('p')
-    p.innerHTML = top
+
+
+    let topText = document.createElement('p');
+    topText.innerHTML = top;
+    topText.classList.add('text');
+    topText.id = 'top'
+
+    let bottomText = document.createElement('p');
+    bottomText.innerHTML = bottom;
+    bottomText.classList.add('text');
+    bottomText.id = 'bottom'
 
     memeArea.insertAdjacentElement('beforeend', newDiv);
     newDiv.insertAdjacentElement('beforeend', image);
-    newDiv.insertAdjacentElement('beforeend', p);
+    newDiv.insertAdjacentElement('beforeend', topText);
+    newDiv.insertAdjacentElement('beforeend', bottomText);
 
     document.querySelector('#url').value = '';
     document.querySelector('#top_text').value = '';
